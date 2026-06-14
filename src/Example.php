c912252a26f82af241a9866b9470bbb4836f1230
LeyreOrtiz <ortiz.161784@e.unavarra.es>
2026-06-14 19:43:08 +0200

    [refactor] - Crear clase con ejemplo

---
+++ src/Example.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ExamenRecuVvs;
+
+class Example
+{
+    public function hello(): string
+    {
+        return 'Hello World!';
+    }
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ExamenRecuVvs\Test;
+
+use ExamenRecuVvs\Example;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testExample()
+    {
+        // Arrange
+        $example = new Example();
+        // Act
+        $result = $example->hello();
+        // Assert
+        $this->assertEquals('Hello World!', $result);
+    }
+
+}
