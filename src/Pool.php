42f771efb30197835a44284c57ce79e38cb4f350
LeyreOrtiz <ortiz.161784@e.unavarra.es>
2026-06-15 14:07:53 +0200

    [verde] - Devuelve que ha acertado 1 partido

--- src/Pool.php
+++ src/Pool.php
@@ -6,10 +6,12 @@ class Pool
 {
     private const ACTION_BET = 'apostar';
     private const ACTION_DELETE = 'quitar';
+    private const ACTION_HIT = 'aciertos';
     private const SIGNS = ['1', '2', 'X'];
     private const MSG_BET_NOT_EXISTS = 'La apuesta seleccionada no existe';
     private const MSG_POOL_CLEARED = 'La quiniela está vacía';
     private const MSG_INVALID_SIGN = 'Signo no válido';
+    private const MSG_HITS = 'Aciertos: ';
     private array $bets = [];
     private Results $results;
     public function __construct(Results $results)
@@ -22,6 +24,17 @@ class Pool
         $instructionAction = $instructionSplitted[0] ?? null;
         $match = $instructionSplitted[1] ?? null;
         $sign = $instructionSplitted[2] ?? null;
+
+        if (strcasecmp($instructionAction, self::ACTION_HIT) === 0) {
+            $cont = 1;
+            foreach ($this->bets as $bet => $signBet) {
+                if ($this->results->getResult($bet) === $signBet) {
+                    $cont = $cont + 1;
+                }
+            }
+            return self::MSG_HITS . $cont;
+        }
+
         if (strcasecmp($instructionAction, self::ACTION_DELETE) === 0) {
             return $this->deleteMatch($match);
         } elseif (strcasecmp($instructionAction, self::ACTION_BET) === 0) {
